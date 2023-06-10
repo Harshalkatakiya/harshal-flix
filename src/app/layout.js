1dcd1dd9a1c61ed7f768738327074f44e6753434
Harshal <130819757+Harshalkatakiya@users.noreply.github.com>
2023-06-10 11:48:02 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-<Analytics />
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
