31c6474afc0de525ff6f87b7f174ae5cfda4b409
Harshalkatakiya <katakiyaharshl001@gmail.com>
2023-06-10 11:34:57 +0530

    updated

--- src/pages/api/fetchmovie.js
+++
@@ -1,7 +0,0 @@
-import { connectToDatabase } from '../utils/mongodb';
-export default async function handler(req, res) {
-    const { db } = await connectToDatabase();
-    const collection = db.collection('movie');
-    const data = await collection.find({}).toArray();
-    res.status(200).json(data);
-}
