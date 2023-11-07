f61a6a9a9e0481e7d42c3b4683b4194b6bce9b49
asdfgdghfjj <katakiyaharshl001@gmail.com>
2023-11-07 20:06:20 +0530

    alert updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import { useEffect } from "react";
 import HarshalFlix from "./harshalflix/page";
 
 const page = () => {
-  alert("Access movies using your Atmiya University gmail only! Example: 156121⁕⁕⁕⁕@atmiyauni.edu.in");
+  useEffect(() => {
+    alert("Access movies using your Atmiya University gmail only! Example: 156121⁕⁕⁕⁕@atmiyauni.edu.in");
+  }, []);
   return (
     <>
       <HarshalFlix />
