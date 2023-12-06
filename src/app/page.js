e5a9fbd2d3752834040c2192676f0b2e9ea9bc52
Harshalkatakiya <katakiyaharshl001@gmail.com>
2023-12-06 19:17:19 +0530

    Add editor settings and update color theme

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,6 @@
-'use client'
-import { useEffect } from "react";
 import HarshalFlix from "./harshalflix/page";
 
 const page = () => {
-  useEffect(() => {
-    alert("Access movies using your Atmiya University gmail only! Example: 156121⁕⁕⁕⁕@atmiyauni.edu.in");
-  }, []);
   return (
     <>
       <HarshalFlix />
